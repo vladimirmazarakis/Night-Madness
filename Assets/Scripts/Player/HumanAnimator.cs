16e2e35e884abc3c931954459cd7519b3cbfea74
Vladimir Mazarakis <vladimirmazarakis@hotmail.com>
2021-12-28 13:56:27 +0200

    Several updates.
    Updates: Resize Human Collider On Runtime,  Smooth Human Normal To Knocked Animations, Different Human Sounds While Knocked.

--- Assets/Scripts/Player/HumanAnimator.cs
+++ Assets/Scripts/Player/HumanAnimator.cs
@@ -10,6 +10,8 @@ public class HumanAnimator : MonoBehaviour
     [SerializeField] private float _animationLayerSmoothTime = 10f;
     private HumanMovement _humanMovement;
     private HumanController _controller;
+    private bool _isKnocked = false;
+    private float _knockedWeight = 0;
     private void Start()
     {
         _humanMovement = GetComponent<HumanMovement>();
@@ -39,16 +41,19 @@ public class HumanAnimator : MonoBehaviour
             _animator.SetBool("IsMoving", _humanMovement.IsMoving);
             _animator.SetBool("IsSprinting", _humanMovement.IsSprinting);
         }
+        if(_isKnocked && _knockedWeight != 1) 
+        {
+            var knockedLayerIndex = _animator.GetLayerIndex("Knocked");
+            _knockedWeight = Mathf.Lerp(_knockedWeight, 1.0f, _animationLayerSmoothTime * Time.deltaTime);
+            _animator.SetLayerWeight(knockedLayerIndex, _knockedWeight);
+        }
     }
 
     #region EventHandling
     private void OnHumanGotKnocked(object sender, EventArgs e)
     {
         Debug.Log("OnHumanGotKnocked event fired.");
-        var knockedLayerIndex = _animator.GetLayerIndex("Knocked");
-        var baseLayerIndex = _animator.GetLayerIndex("Base");
-        _animator.SetLayerWeight(knockedLayerIndex, 1);
-        _animator.SetLayerWeight(baseLayerIndex,0);
+        _isKnocked = true;
     }
     #endregion
 }
--- Assets/Scripts/Sound/PlayerHumanAudio.cs
+++ Assets/Scripts/Sound/PlayerHumanAudio.cs
@@ -10,15 +10,19 @@ public class PlayerHumanAudio : MonoBehaviour
     [Header("Wood")]
     [SerializeField] private List<AudioClip> _woodSteps;
     [SerializeField] private List<AudioClip> _woodRunningSteps;
+    [SerializeField] private List<AudioClip> _woodKnockedSteps;
     [Header("Grass")]
     [SerializeField] private List<AudioClip> _grassSteps;
     [SerializeField] private List<AudioClip> _grassRunningSteps;
+    [SerializeField] private List<AudioClip> _grassKnockedSteps;
     [Header("Gravel")]
     [SerializeField] private List<AudioClip> _gravelSteps;
     [SerializeField] private List<AudioClip> _gravelRunningSteps;
+    [SerializeField] private List<AudioClip> _gravelKnockedSteps;
     [Header("Dirt")]
     [SerializeField] private List<AudioClip> _dirtSteps;
     [SerializeField] private List<AudioClip> _dirtRunningSteps;
+    [SerializeField] private List<AudioClip> _dirtKnockedSteps;
     #endregion
     [Header("Other settings")]
     [Range(0,100)][SerializeField] private float _stepsSoundSmooth = 0.3f;
@@ -26,11 +30,21 @@ public class PlayerHumanAudio : MonoBehaviour
     private AudioSource _audioSource;
     private bool _playingSound = false;
     private HumanMovement _movement;
+    private HumanController _controller;
+    private bool _isKnocked = false;
     private void Start()
     {
         _audioSource = GetComponent<AudioSource>();
         _movement = GetComponent<HumanMovement>();
+        _controller = GetComponent<HumanController>();
+        _controller.gotKnocked += OnHumanGotKnocked;
     }
+
+    private void OnHumanGotKnocked(object sender, System.EventArgs e)
+    {
+        _isKnocked = true;
+    }
+
     private void Update()
     {
         if (!_movement.IsMoving && _audioSource.volume > 0)
@@ -53,25 +67,56 @@ public class PlayerHumanAudio : MonoBehaviour
             _playingSound = true;
             switch (_gameObjectTag)
             {
-
                 case "Wood":
-                    randomIndex = _movement.IsSprinting ? random.Next(_woodRunningSteps.Count) : random.Next(_woodSteps.Count);
-                    _audioSource.clip = _movement.IsSprinting ? _woodRunningSteps[randomIndex] : _woodSteps[randomIndex];
+                    if (_isKnocked)
+                    {
+                        randomIndex = random.Next(_woodKnockedSteps.Count);
+                        _audioSource.clip = _woodKnockedSteps[randomIndex];
+                    }
+                    else
+                    {
+                        randomIndex = _movement.IsSprinting ? random.Next(_woodRunningSteps.Count) : random.Next(_woodSteps.Count);
+                        _audioSource.clip = _movement.IsSprinting ? _woodRunningSteps[randomIndex] : _woodSteps[randomIndex];
+                    }
                     _audioSource.Play();
                     break;
                 case "Grass":
-                    randomIndex = _movement.IsSprinting ? random.Next(_grassRunningSteps.Count) : random.Next(_grassSteps.Count);
-                    _audioSource.clip = _movement.IsSprinting ? _grassRunningSteps[randomIndex] : _grassSteps[randomIndex];
+                    if (_isKnocked)
+                    {
+                        randomIndex = random.Next(_grassKnockedSteps.Count);
+                        _audioSource.clip = _grassKnockedSteps[randomIndex];
+                    }
+                    else
+                    {
+                        randomIndex = _movement.IsSprinting ? random.Next(_grassRunningSteps.Count) : random.Next(_grassSteps.Count);
+                        _audioSource.clip = _movement.IsSprinting ? _grassRunningSteps[randomIndex] : _grassSteps[randomIndex];
+                    }
                     _audioSource.Play();
                     break;
                 case "Gravel":
+                    if (_isKnocked)
+                    {
+                        randomIndex = random.Next(_gravelKnockedSteps.Count);
+                        _audioSource.clip = _gravelKnockedSteps[randomIndex];
+                    }
+                    else 
+                    {
                     randomIndex = _movement.IsSprinting ? random.Next(_gravelRunningSteps.Count) : random.Next(_gravelSteps.Count);
                     _audioSource.clip = _movement.IsSprinting ? _gravelRunningSteps[randomIndex] : _gravelSteps[randomIndex];
+                    }
                     _audioSource.Play();
                     break;
                 case "Dirt":
-                    randomIndex = _movement.IsSprinting ? random.Next(_dirtRunningSteps.Count) : random.Next(_dirtSteps.Count);
-                    _audioSource.clip = _movement.IsSprinting ? _dirtRunningSteps[randomIndex] : _dirtSteps[randomIndex];
+                    if (_isKnocked)
+                    {
+                        randomIndex = random.Next(_dirtKnockedSteps.Count);
+                        _audioSource.clip = _dirtKnockedSteps[randomIndex];
+                    }
+                    else
+                    {
+                        randomIndex = _movement.IsSprinting ? random.Next(_dirtRunningSteps.Count) : random.Next(_dirtSteps.Count);
+                        _audioSource.clip = _movement.IsSprinting ? _dirtRunningSteps[randomIndex] : _dirtSteps[randomIndex];
+                    }
                     _audioSource.Play();
                     break;
             }
